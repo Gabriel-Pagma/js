36dab1e926bf495958138123709e5ce760d25986
Gabriel Arantes <gabrielaranteslemes@gmail.com>
2023-12-15 15:51:47 -0300

    condições if else

---
+++ aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 50.5
+console.log(`A velocidade do seu carro é de : ${vel}km/h`)
+if (vel >60) { // condição simples
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO! `)
+}
+console.log(`dirija sempre usando cinto de seguraça!`)
---
+++ aula011/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Russia'
+console.log(`Vivendo no ${pais}`)
+if (pais == 'Brasil') {
+    console.log(`Você é Brasileiro!`)
+} else {
+    console.log(`Você é estrangeiro`)
+}
